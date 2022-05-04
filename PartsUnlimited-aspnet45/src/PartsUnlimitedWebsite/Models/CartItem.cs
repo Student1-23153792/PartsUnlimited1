0cde0aabfc48a2bc798e56d488e8f7687b5ff737
Student1-23153792 <Student1-23153792@lodsasdoutlook.onmicrosoft.com>
2022-05-03 20:36:18 -0700

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //My first comments
+        //My second change
         [Key]
         public int CartItemId { get; set; }
 
